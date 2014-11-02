4865ed1c8965fe5210d965c0dda503f2931da836
tinqueija <tinqueija@gmail.com>
2014-11-02 21:43:23 +0000

    finalizado el metodo para iniciar terremoto en cierta coordenada que decrementa vida en todos los lotes por un factor relacionado con la distancia al epicentro

--- test/fiuba/algo3/tp2/TerremotoTest.java
+++ test/fiuba/algo3/tp2/TerremotoTest.java
@@ -3,5 +3,21 @@ import junit.framework.Assert;
 import org.junit.Test;
 
 public class TerremotoTest {
-
+	
+	@Test
+	public void TerremotoDeberiaDestruirPorCompletoLaConstruccionEnElEpicentro(){
+		Mapa unMapa = new Mapa();
+		Terremoto unTerremoto = new Terremoto();
+		Construccion casa = new Casa();
+		Construccion otraCasa = new Casa();
+		Construccion ultimaCasa = new Casa();
+		unMapa.insertarConstruccionEn(5, 5, casa);
+		unMapa.insertarConstruccionEn(3, 5, otraCasa); //Distancia de 2 lotes entonces vida decrementa por 100-(1.5x2)=97
+		unMapa.insertarConstruccionEn(4, 2, ultimaCasa); //sqrt(1+9)=3.16 entonces la vida decrementa por 100-(1.5x3)=95.5
+		unTerremoto.iniciarTerremotoEn(5, 5, unMapa);
+		
+		Assert.assertEquals(0.0, unMapa.obtenerVidaDeConstruccionEnLote(5,5));
+		Assert.assertEquals(100.0-97.0, unMapa.obtenerVidaDeConstruccionEnLote(3,5));
+		Assert.assertEquals(100.0-95.5, unMapa.obtenerVidaDeConstruccionEnLote(4, 2));
+	}
 }
