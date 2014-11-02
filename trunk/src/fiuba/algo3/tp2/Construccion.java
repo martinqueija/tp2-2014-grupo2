e485b26808a537415b188c4a97689e07b3ee0866
tinqueija@gmail.com <tinqueija@gmail.com@f41287c7-b400-5f97-e00b-be88e2d0a773>
2014-11-02 18:53:25 +0000

    se modifico la clase construccion

--- trunk/src/fiuba/algo3/tp2/Construccion.java
+++ trunk/src/fiuba/algo3/tp2/Construccion.java
@@ -2,12 +2,21 @@ package fiuba.algo3.tp2;
 
 public class Construccion {
 	int vida = 100;
+	boolean enPie = true;
 	
 	public void decrementarVida(int factor){
 		vida = vida - factor;
+		if (vida <= 0) {
+			vida = 0;
+			enPie = false;
+		}
 	}
 	
 	public int obtenerVida(){
 		return vida;
+	}
+	
+	public boolean obtenerEstado(){
+		return enPie;
 	}
 }
