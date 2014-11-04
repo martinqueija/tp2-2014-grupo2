a032680117162b738ca87b9c215b5e3a73eb92ef
fedeaxelrud <fedeaxelrud@gmail.com>
2014-11-04 02:15:54 +0000

    Agrega la clase agua y aguaTest

---
+++ src/fiuba/algo3/tp2/Agua.java
@@ -0,0 +1,15 @@
+package fiuba.algo3.tp2;
+
+public class Agua extends Superficie {
+	
+	public boolean esAgua() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	public boolean esTerrenoLLano() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
