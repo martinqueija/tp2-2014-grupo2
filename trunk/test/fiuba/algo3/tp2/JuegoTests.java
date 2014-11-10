ed27e011113e7ae621d0e162a8557f2e90ef25b3
tinqueija@gmail.com <tinqueija@gmail.com@f41287c7-b400-5f97-e00b-be88e2d0a773>
2014-11-10 18:01:26 +0000

    implementada clase Juego y sus tests

---
+++ trunk/test/fiuba/algo3/tp2/JuegoTests.java
@@ -0,0 +1,34 @@
+package fiuba.algo3.tp2;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class JuegoTests {
+	
+	@Test
+	public void JuegoDeberiaInicializarseConPoblacionCero(){
+		Juego elJuego = new Juego();
+		Assert.assertEquals(0,elJuego.laPoblacion.getCantidadPoblacion());
+	}
+	
+	@Test
+	public void JuegoDeberiaInicializarseConSaldo1000(){
+		Juego elJuego = new Juego();
+		Assert.assertEquals(1000, elJuego.laCaja.ObtenerSaldo());
+	}
+	
+	@Test
+	public void JuegoDeberiaInicializarseConMapaVacio(){
+		Juego elJuego = new Juego();
+		boolean vale = true;
+		Lote loteTemp;
+		for (int i=0;i<elJuego.elMapa.obtenerTamanioLado();i++){
+			for (int j=0;j<elJuego.elMapa.obtenerTamanioLado();j++){
+				loteTemp = elJuego.elMapa.obtenerLote(i, j);
+				if(loteTemp.hayConstruccion()) { vale = false; }
+			}
+		}
+		Assert.assertEquals(true,vale);
+	}
+}
