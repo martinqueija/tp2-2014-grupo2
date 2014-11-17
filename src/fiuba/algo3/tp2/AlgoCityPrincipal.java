ac524b5270e4b837b32e6fba7503050e648ad804
tinqueija <tinqueija@gmail.com>
2014-11-17 18:26:03 +0000

    mas tests para lo identificadores de cada lote. contunuacion de la implementacion de la vista

--- src/fiuba/algo3/tp2/AlgoCityPrincipal.java
+++ src/fiuba/algo3/tp2/AlgoCityPrincipal.java
@@ -1,28 +1,35 @@
 package fiuba.algo3.tp2;
 import java.awt.EventQueue;
-
 import fiuba.algo3.tp2.Controlador.AlgoCityControlador;
 import fiuba.algo3.tp2.Vista.AlgoCityVista;
 
 public class AlgoCityPrincipal {
-	
-	AlgoCityVista view;
-	Juego juego;
-	AlgoCityControlador controlador;
 
-	public void main(String[] args){
+	static AlgoCityVista view;
+	static Juego juego;
+	static AlgoCityControlador controlador;
 
+
+	public static void main(String[] args) {
+
+		
+		
 		Runnable runner = new Runnable(){
 			@Override
 			public void run(){
 				view = new AlgoCityVista(17, 17);
 				view.setVisible(true);
 			}
-		};
-		EventQueue.invokeLater(runner);
-		juego = new Juego();
-		controlador = new AlgoCityControlador(view, juego);
+			};
+			EventQueue.invokeLater(runner);
+			juego = new Juego();
+			controlador = new AlgoCityControlador(view, juego);
 	}
+		
+		
+		
+		
+	
 	
 
 }
