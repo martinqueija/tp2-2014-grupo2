ba538cd9b1d4baa743799c5c68e481fe34ea2099
tinqueija <tinqueija@gmail.com>
2014-11-24 16:55:01 +0000

    re Factoring

--- src/fiuba/algo3/tp2/AdministradorCatastrofes.java
+++ src/fiuba/algo3/tp2/AdministradorCatastrofes.java
@@ -3,15 +3,18 @@ package fiuba.algo3.tp2;
 public class AdministradorCatastrofes {
 	
 	
-	public void actuar(Mapa elMapa, RandomizadorInterface randomizer){
+	public boolean actuar(Mapa elMapa, RandomizadorInterface randomizer){
+		boolean huboCatastrofe = false;
 		int numRandom = randomizer.obtenerNumeroRandomCat(3);
 		if (numRandom == 2) {
+			huboCatastrofe = true;
 			Terremoto unTerremoto = new Terremoto();
 			int xRand = randomizer.obtenerNumeroRandomX(elMapa.obtenerTamanioLado());
 			int yRand = randomizer.obtenerNumeroRandomY(elMapa.obtenerTamanioLado());
 			unTerremoto.iniciarTerremotoEn(xRand, yRand, elMapa);
 		}
 		if (numRandom == 1) {
+			huboCatastrofe = true;
 			Godzilla nuevoGodzilla = new Godzilla();
 			int ladoDeInicio = randomizer.obtenerNumeroRandomCat(4);
 			int ladoDeFinalizacion = randomizer.obtenerNumeroRandomCat(4);
@@ -20,7 +23,7 @@ public class AdministradorCatastrofes {
 			nuevoGodzilla.iniciarGodzilla(ladoDeInicio,ladoDeFinalizacion,alturaDeInicio,alturaDeFinalizacion,elMapa);
 			}
 
-
+		return huboCatastrofe;
 	}
 	
 	
---
+++ src/fiuba/algo3/tp2/Controlador/LoteParaVista.java
@@ -0,0 +1,11 @@
+package fiuba.algo3.tp2.Controlador;
+
+public class LoteParaVista {
+	
+	boolean tieneLineaDeTension = false;
+	boolean tieneTuberia = false;
+	boolean tieneElectricidad = false;
+	//tipoConstruccion construccion;
+	
+
+}
